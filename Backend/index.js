ab7fb04e322809034d007aa6b27a666ae58d0c7b
Jatin Gupta <jatinji890@gmail.com>
2026-02-26 12:10:37 +0530

    Commit4

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 4000;
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://socialhub3.netlify.app/",
+    origin: "https://socialhub3.netlify.app",
     credentials: true,
   })
 );
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }))
 const io = new Server(server, {
     cors: {
         // origin: "http://localhost:5173",
-        origin: "https://socialhub3.netlify.app/",
+        origin: "https://socialhub3.netlify.app",
         credentials: true,
     }
 })
